4a8cc94eac7251aa072cb7b1d2a362d273d04c2d
ritomar <ritomar@hotmail.com>
2019-01-21 21:31:29 -0300

    Adicionado template index.html

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 
+def get_url():
+    base_url = "https://developers.tagplus.com.br/authorize?response_type=code"
+    return base_url + "&client_id=d5d295a844634ad0b423f5e2fb5151ae&scope=read:pedidos"
+
+
 def home(r):
-    return render(r, 'core/index.html')
+    return render(r, 'core/index.html', {"url": get_url()})
